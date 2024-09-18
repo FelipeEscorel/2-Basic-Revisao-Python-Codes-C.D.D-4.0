7d243d8595645d9a65e0491a5131f1d597939f9c
Felipe Escorel <felipeescorel@hotmail.com>
2024-09-18 09:51:52 -0300

    Create revisao004.py

---
+++ revisao004.py
@@ -0,0 +1,8 @@
+A = int(input("Digite um valor inteiro: "))
+B = int(input("Digite outro valor inteiro: "))
+if A == B:
+    C = A + B
+    print(f"Os valores de A({A}) e B({B}) são iguais. Logo a soma dos dois é {C}.")
+else:
+    C = A * B
+    print(f"Os valores de A({A}) e B({B}) são diferentes. Logo a multiplicaçao dos dois é {C}.")
