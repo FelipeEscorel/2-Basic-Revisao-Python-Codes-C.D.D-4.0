72e12a8cd4b3c2acaf7bac343a291e2dbe102d68
Felipe Escorel <felipeescorel@hotmail.com>
2024-09-18 08:44:45 -0300

    Create revisao001.py

---
+++ revisao001.py
@@ -0,0 +1,10 @@
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+soma = A + B
+
+if soma >= C:
+    print(f"A soma de A + B é {soma}")
+else:
+    print(f"A soma de A + B é {soma} e é menor que C ({C})")
